28afe45ff8594f27aeb44e5d476e20659be45e3b
Charles Betz <betz4871@stthomas.edu>
2018-12-08 02:30:13 +0000

    deac updates

--- deac.sh
+++ deac.sh
@@ -3,13 +3,18 @@
 #az vm deallocate -g myResourceGroup -n svc1-vm1
 #az vm delete -g myResourceGroup -n svc1-vm1
 
+echo "removing tfstate cookies"
 rm terraform.tfstate*
 
+echo "deallocating VM"
 az vm deallocate -g myResourceGroup -n  myVm
-az vm delete -g myResourceGroup -n  myVm
-
-az network nic delete -g myResourceGroup -n myNIC
+echo "deleting resource group"
+az vm delete -g myResourceGroup -n  myVm --yes
+echo "deleting nic"
+az network nic delete -g myResourceGroup -n myNIC 
+echo "deleting vnet"
 az network vnet delete -g myResourceGroup -n myVnet
-az disk delete -g myResourceGroup -n myOsDisk
+echo "deleting disk"
+az disk delete -g myResourceGroup -n myOsDisk  --yes
 
 # delete storage
